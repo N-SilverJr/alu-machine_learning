63447fadcf6e33f114a25f3e74e26fe4740ad7c9
N-SilverJr <s.nshuti@alustudent.com>
2026-02-04 14:15:39 +0200

    Made files executable

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,7 +3,6 @@
 Module for transposing a 2D matrix
 """
 
-
 def matrix_transpose(matrix):
     """
     Returns the transpose of a 2D matrix
@@ -14,18 +13,6 @@ def matrix_transpose(matrix):
     Returns:
         list: New transposed matrix
     """
-    # Get dimensions of the matrix
-    rows = len(matrix)
-    cols = len(matrix[0])
-    
-    # Create empty transposed matrix
-    transpose = []
-    
-    # For each column in original matrix, create a new row in transpose
-    for col in range(cols):
-        new_row = []
-        for row in range(rows):
-            new_row.append(matrix[row][col])
-        transpose.append(new_row)
-    
-    return transpose
+    # Using list comprehension
+    return [[matrix[row][col] for row in range(len(matrix))] 
+            for col in range(len(matrix[0]))]
