aa72be43fc309fb728e8611670bcfa9fa375e444
N-SilverJr <s.nshuti@alustudent.com>
2026-02-04 14:23:26 +0200

    Made files executable

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-'''
-    def matrix_shape(matrix) calculates the shape of a matrix
-'''
+"""
+Module for calculating the shape of a matrix
+"""
 
 
 def matrix_shape(matrix):
-
-    mat_shape = []
-    while isinstance(matrix, list):
-        mat_shape.append(len(matrix))
-        matrix = matrix[0]
-    return mat_shapie
+    """
+    Calculates the shape of a matrix
+    """
+    shape = []
+    current = matrix
+    while isinstance(current, list):
+        shape.append(len(current))
+        current = current[0] if current else None
+    return shape
